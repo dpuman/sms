e5e856c8f2d15a6d8bef50ee40434849c19f8414
Dpu <dpumail.in@gmail.com>
2022-07-04 19:58:21 +0600

    about us and story done

--- ngo/views.py
+++ ngo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-# Create your views here.
 
 
 class MyHome(TemplateView):
     template_name = "ngo/home.html"
+
+
+class About(TemplateView):
+    template_name = "ngo/about.html"
