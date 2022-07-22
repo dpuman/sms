b79a2156e7f75f24f0547996b4e2ef711a278b4c
Dpu <dpumail.in@gmail.com>
2022-07-23 03:39:41 +0600

    projects added ongoing and previous

--- ngo/urls.py
+++ ngo/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("about/", views.About.as_view(), name="about"),
     path("donor-partnership/", views.DonorPartnership.as_view(),
          name="donor_partnership"),
+    path("projects/<str:type>", views.Projects.as_view(),
+         name="projects"),
+    path("project-details/<int:pk>", views.ProjectDetails.as_view(),
+         name="project_details"),
 
 ]
--- ngo/views.py
+++ ngo/views.py
@@ -13,3 +13,20 @@ class About(TemplateView):
 
 class DonorPartnership(TemplateView):
     template_name = "ngo/donor-partnership.html"
+
+
+class Projects(TemplateView):
+    template_name = "ngo/projects.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # for Dynamic url argument value
+        type = kwargs
+        myType = context["type"]
+
+        return context
+
+
+class ProjectDetails(TemplateView):
+    template_name = "ngo/projects-details.html"
